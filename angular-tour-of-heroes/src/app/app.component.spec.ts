0c8c1368b17b662fc5966d3426c4f7fa96b0bbc8
Jianfeng <jeff.doyle@foxmail.com>
2018-02-27 20:35:57 +0800

    Tour-of-heroes: add hero-detail component

--- angular-tour-of-heroes/src/app/app.component.spec.ts
+++ angular-tour-of-heroes/src/app/app.component.spec.ts
@@ -2,13 +2,15 @@ import { TestBed, async } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 describe('AppComponent', () => {
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			declarations: [
 				AppComponent,
-				HeroesComponent
+				HeroesComponent,
+				HeroDetailComponent
 			],
 			imports: [FormsModule]
 		}).compileComponents();

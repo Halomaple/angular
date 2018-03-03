e81a59ef93851135d0cb26843fe29563ecdc3054
Jianfeng <jeff.doyle@foxmail.com>
2018-03-03 18:39:43 +0800

    Tour-of-heroes: add dashboard

--- angular-tour-of-heroes/src/app/app.component.spec.ts
+++ angular-tour-of-heroes/src/app/app.component.spec.ts
@@ -9,34 +9,47 @@ import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 describe('AppComponent', () => {
-	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [
-				AppComponent,
-				HeroesComponent,
-				HeroDetailComponent,
-				MessagesComponent,
-			],
-			providers: [HeroService, MessageService, { provide: APP_BASE_HREF, useValue: '/' }],
-			imports: [FormsModule, AppRoutingModule]
-		}).compileComponents();
-	}));
-	it('should create the app', async(() => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.debugElement.componentInstance;
-		expect(app).toBeTruthy();
-	}));
-	it(`should have as title 'Tour of Heroes'`, async(() => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.debugElement.componentInstance;
-		expect(app.title).toEqual('Tour of Heroes');
-	}));
-	it('should render title in a h1 tag', async(() => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.debugElement.nativeElement;
-		expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
-	}));
+	beforeEach(
+		async(() => {
+			TestBed.configureTestingModule({
+				declarations: [
+					AppComponent,
+					HeroesComponent,
+					HeroDetailComponent,
+					MessagesComponent,
+					DashboardComponent
+				],
+				providers: [HeroService, MessageService, { provide: APP_BASE_HREF, useValue: '/' }],
+				imports: [FormsModule, AppRoutingModule]
+			}).compileComponents();
+		})
+	);
+	it(
+		'should create the app',
+		async(() => {
+			const fixture = TestBed.createComponent(AppComponent);
+			const app = fixture.debugElement.componentInstance;
+			expect(app).toBeTruthy();
+		})
+	);
+	it(
+		`should have as title 'Tour of Heroes'`,
+		async(() => {
+			const fixture = TestBed.createComponent(AppComponent);
+			const app = fixture.debugElement.componentInstance;
+			expect(app.title).toEqual('Tour of Heroes');
+		})
+	);
+	it(
+		'should render title in a h1 tag',
+		async(() => {
+			const fixture = TestBed.createComponent(AppComponent);
+			fixture.detectChanges();
+			const compiled = fixture.debugElement.nativeElement;
+			expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
+		})
+	);
 });

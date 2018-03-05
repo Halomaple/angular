fbf8037192f092b2a63de8ea166dbce6cbbcb51e
Jianfeng <jeff.doyle@foxmail.com>
2018-03-05 23:26:11 +0800

    Tour-of-heroes: fix and add more behaviour tests

--- angular-tour-of-heroes/e2e/app.e2e-spec.ts
+++ angular-tour-of-heroes/e2e/app.e2e-spec.ts
@@ -8,12 +8,25 @@ describe('angular-tour-of-heroes App', () => {
 	});
 
 	it('should display welcome message', () => {
-		page.navigateTo();
+		page.navigateToMain();
 		expect(page.getParagraphText()).toEqual('Tour of Heroes');
 	});
 
-	it(`should display hero list`, () => {
-		page.navigateTo();
-		expect(page.getHeroListTitleText()).toContain('My Heroes');
+	it(`should display hero dashboard with top heroes`, () => {
+		page.navigateToMain();
+		expect(page.getHeroDashboardTitleText()).toContain('Top Heroes');
+		expect(page.getTopHeroesCount()).toEqual(4);
+	});
+
+	it(`should be able to navigate to hero detail by clicking name item`, () => {
+		page.navigateToMain();
+		page.clickFirstHeroItem();
+		expect(page.getHeroDetailTitle()).toEqual('NARCO Details');
+	});
+
+	it(`should be able to navigate to heroes list`, () => {
+		page.clickHeroesListButton();
+		expect(page.getHeroesListTitle()).toEqual('My Heroes');
+		expect(page.getHeroesCount()).toEqual(10);
 	});
 });
--- angular-tour-of-heroes/e2e/app.po.ts
+++ angular-tour-of-heroes/e2e/app.po.ts
@@ -1,7 +1,7 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-	navigateTo() {
+	navigateToMain() {
 		return browser.get('/');
 	}
 
@@ -9,7 +9,31 @@ export class AppPage {
 		return element(by.css('app-root h1')).getText();
 	}
 
-	getHeroListTitleText() {
+	getHeroDashboardTitleText() {
+		return element(by.css('app-dashboard h3')).getText();
+	}
+
+	getTopHeroesCount() {
+		return element.all(by.css('app-dashboard div a')).count();
+	}
+
+	getHeroDetailTitle() {
+		return element(by.css('app-hero-detail div h2')).getText();
+	}
+
+	clickFirstHeroItem() {
+		element(by.css('app-dashboard div a:first-child')).click();
+	}
+
+	clickHeroesListButton() {
+		element(by.css('app-root nav a:last-child')).click();
+	}
+
+	getHeroesListTitle() {
 		return element(by.css('app-heroes h2')).getText();
+	}
+
+	getHeroesCount() {
+		return element.all(by.css('app-heroes ul li')).count();
 	}
 }
